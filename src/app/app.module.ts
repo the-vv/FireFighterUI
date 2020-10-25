e673c95a27fa9b99807569ad56240fe592783604
vv-2001 <vishnuvinod2772001@gmail.com>
2020-10-25 18:03:04 +0530

    divided into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StatusComponent } from './status/status.component';
+import { ManualComponent } from './manual/manual.component';
+import { TerminalComponent } from './terminal/terminal.component';
+import { VideoComponent } from './video/video.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StatusComponent,
+    ManualComponent,
+    TerminalComponent,
+    VideoComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
