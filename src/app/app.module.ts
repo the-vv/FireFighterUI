b79689f9b40a12cce3bfaafa8e482123475c4631
vv-2001 <vishnuvinod2772001@gmail.com>
2020-11-10 23:47:45 +0530

    adding features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { TerminalComponent } from './terminal/terminal.component';
 import { VideoComponent } from './video/video.component';
 import { MapComponent } from './map/map.component';
 import { ServerService } from './server.service';
+// import { SafePipe } from './safepipe'
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
  
@@ -31,7 +32,7 @@ const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
     FormsModule,
     AppRoutingModule,
     HttpClientModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
   ],
   providers: [
     ServerService
--- src/app/server.service.ts
+++ src/app/server.service.ts
@@ -9,11 +9,25 @@ export class ServerService {
 
   mainStatus: status;
   terminalLog: string = '';
+  videoUrl = ''
 
-  constructor(private socket: Socket) {
+  constructor(
+    private socket: Socket
+    ) {
+    socket.on('connect', () => {
+      console.log('connected'); // true
+    });
+    socket.on('disconnect', () => {
+      console.log('Disconnected'); // false
+      // location.href = 'https://firefighteronline.herokuapp.com'
+    });
     socket.on('status', (data) => {
       console.log(data);
       this.mainStatus = data
+    })
+    socket.on('videoUrl', (data) =>{
+      console.log('Video: ', data);      
+      this.videoUrl = data;
     })
     socket.on('terminalLog', (data) =>{
       if(data){
@@ -21,6 +35,13 @@ export class ServerService {
         this.terminalLog = this.terminalLog + data + '\n'
         let msgContainer = document.getElementById("terminal-display");           
         msgContainer.scrollTop = msgContainer.scrollHeight;
+      }
+    })
+    this.socket.on('newSession', () =>{
+      if(confirm('Session disconnected due to another session\nclick ok to reconnect')){
+        location.reload()
+      }else{
+        // location.href = 'https://firefighteronline.herokuapp.com'
       }
     })
   }
